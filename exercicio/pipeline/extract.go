a148284be554c42d60767a907d267c99e8ec6bac
Gustavo Fantato <gustavo.fernandes@monks.com>
2025-12-22 15:50:55 -0300

    Implemented extractor

--- exercicio/pipeline/extract.go
+++ exercicio/pipeline/extract.go
@@ -1,7 +1,26 @@
 package pipeline
 
+import (
+	"io"
+	"os"
+)
+
 // Extract lê o arquivo do disco
 func Extract(filename string) ([]byte, error) {
 	// TODO: Implementar leitura de arquivo
-	return nil, nil
+	file, err := os.Open(filename);
+
+	if err != nil{
+		return nil, err; 
+	}
+
+	defer file.Close()
+
+	data, err := io.ReadAll(file);
+
+	if err != nil{
+		return nil, err; 
+	}
+
+	return data, nil;
 }
