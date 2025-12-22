38aa63453bde886dae0a8203681a32113920ec5f
caiopires-monks <caio.pires@monks.com>
2025-12-22 15:47:19 -0300

    Implementa extract

--- exercicio/main.go
+++ exercicio/main.go
@@ -1,34 +1,45 @@
 package main
 
 import (
+	"exercicio/pipeline"
 	"fmt"
-//	"log"
-	
-//	"exercicio/pipeline"
+	"log"
+	"os"
+	//	"log"
+	//	"exercicio/pipeline"
 )
 
 func main() {
 	fmt.Println("🚀 Iniciando Pipeline Local...")
-
+	filepath := os.Getenv("FEED_BRUTO_PATH")
+	if filepath == "" {
+		log.Fatal(fmt.Errorf("Variável de ambiente não encontrada ou nula"))
+	}
 	// 1. Extract
-    // TODO: Implementar chamada
+	// TODO: Implementar chamada
 	fmt.Println("1. Lendo arquivo...")
-    // rawData, err := pipeline.Extract("feed_bruto.xml")
+
+	rawData, err := pipeline.Extract(filepath)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+	fmt.Println(string(rawData[:]))
 
 	// 2. Reduce
-    // TODO: Implementar chamada
-	fmt.Println("2. Normalizando dados...")
-    // products, err := pipeline.Reduce(rawData)
+	// TODO: Implementar chamada
+	//fmt.Println("2. Normalizando dados...")
+	// products, err := pipeline.Reduce(rawData)
 
 	// 3. FFT
-    // TODO: Implementar chamada
-	fmt.Println("3. Aplicando regras de negócio (Google)...")
-    // googleProducts := pipeline.FFT(products)
+	// TODO: Implementar chamada
+	// fmt.Println("3. Aplicando regras de negócio (Google)...")
+	// googleProducts := pipeline.FFT(products)
 
 	// 4. Load
-    // TODO: Implementar chamada
-	fmt.Println("4. Gerando arquivo final...")
-    // err := pipeline.Load(googleProducts, "feed_google.xml")
+	// TODO: Implementar chamada
+	// fmt.Println("4. Gerando arquivo final...")
+	// err := pipeline.Load(googleProducts, "feed_google.xml")
 
-	fmt.Println("✅ Processo finalizado com sucesso!")
+	// fmt.Println("✅ Processo finalizado com sucesso!")
 }
--- exercicio/pipeline/extract.go
+++ exercicio/pipeline/extract.go
@@ -1,7 +1,15 @@
 package pipeline
 
+import (
+	"fmt"
+	"os"
+)
+
 // Extract lê o arquivo do disco
 func Extract(filename string) ([]byte, error) {
-	// TODO: Implementar leitura de arquivo
-	return nil, nil
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("Caminho de arquivo inválido")
+	}
+	return data, err
 }
